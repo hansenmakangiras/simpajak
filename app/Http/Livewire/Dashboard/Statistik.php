e5eb91d49a9f5bab8b26f483984f31b95747a28f
Hansen Makangiras <hansenmakangiras@users.noreply.github.com>
2021-08-13 18:21:25 +0000

    Apply fixes from StyleCI

--- app/Http/Livewire/Dashboard/Statistik.php
+++ app/Http/Livewire/Dashboard/Statistik.php
@@ -13,24 +13,21 @@ class Statistik extends Component
     public int $totalVisit = 0;
     public int $paidVisit = 0;
 
-
     public function mount()
     {
-
         $this->total_visit = [21, 9, 36, 12, 44, 25, 59, 41, 66, 25];
         $this->paid_visit = [22, 19, 30, 47, 32, 44, 34, 55, 41, 69];
     }
 
     public function fetchData()
     {
-        $this->totalVisit += rand(0,10);
-        $this->paidVisit += rand(0,10);
+        $this->totalVisit += rand(0, 10);
+        $this->paidVisit += rand(0, 10);
 
-        $totVisit = array_replace($this->total_visit,[10 => $this->totalVisit]);
-        $paidVisit = array_replace($this->paid_visit,[10 => $this->paidVisit]);
+        $totVisit = array_replace($this->total_visit, [10 => $this->totalVisit]);
+        $paidVisit = array_replace($this->paid_visit, [10 => $this->paidVisit]);
 
         $this->emit('refreshStatistik', ['seriesData' => ['totVisit' => $totVisit, 'paidVisit' => $paidVisit]]);
-
     }
 
     public function render()
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,9 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory;
+    use Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
